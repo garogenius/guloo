d2712d2c2e8d1370abbce89c29da6bdd83c2dc5a
mrgenius <net.suleimangaro@gmail.com>
2026-08-18 07:25:09 -0500

    project integration week 3

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: false });
+  app.enableCors({ origin: '*' });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
